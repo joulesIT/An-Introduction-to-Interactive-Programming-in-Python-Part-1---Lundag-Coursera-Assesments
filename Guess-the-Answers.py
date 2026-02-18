ee5f8751dec350b13b1deb9acafb06e1d6eba74b
Juliana Marie Lundag <lundagjuliam@gmail.com>
2026-02-19 01:42:25 +0800

    Refactor game to use SimpleGUI for UI
    Updated the game to use SimpleGUI for a graphical interface.

--- Guess-the-Answers.py
+++ Guess-the-Answers.py
@@ -1,3 +1,4 @@
+import simplegui
 import random
 import math
 
@@ -9,28 +10,28 @@ remaining_guesses = 7
 def new_game():
     global secret_number, remaining_guesses
     secret_number = random.randrange(0, num_range)
-    # Calculate number of guesses: smallest n such that 2**n >= high - low + 1
     remaining_guesses = math.ceil(math.log(num_range + 1, 2))
-    print(f"\nNew game started! Guess a number in range [0, {num_range})")
-    print(f"You have {remaining_guesses} guesses remaining.")
+    print("\nNew game! Guess a number in range [0, " + str(num_range) + ")")
+    print("You have " + str(remaining_guesses) + " guesses remaining.")
 
 def input_guess(guess):
     global remaining_guesses
     guess = int(guess)
     remaining_guesses -= 1
-    print(f"\nGuess was {guess}")
-    print(f"Guesses remaining: {remaining_guesses}")
-
+    print("\nGuess was " + str(guess))
+    
     if guess == secret_number:
         print("Correct! You win!")
         new_game()
     elif remaining_guesses == 0:
-        print(f"Out of guesses! The secret number was {secret_number}.")
+        print("Out of guesses! The secret number was " + str(secret_number) + ".")
         new_game()
     elif guess < secret_number:
         print("Higher")
+        print("Guesses remaining: " + str(remaining_guesses))
     else:
         print("Lower")
+        print("Guesses remaining: " + str(remaining_guesses))
 
 def range_100():
     global num_range
@@ -44,23 +45,12 @@ def range_1000():
     print("Range set to [0, 1000)")
     new_game()
 
-# --- Simple console-based UI (no simplegui) ---
-def main():
-    new_game()
-    print("\nCommands: enter a number to guess, 'r100' for range [0,100), 'r1000' for range [0,1000), 'q' to quit")
-    while True:
-        user_input = input("> ").strip()
-        if user_input == 'q':
-            break
-        elif user_input == 'r100':
-            range_100()
-        elif user_input == 'r1000':
-            range_1000()
-        else:
-            try:
-                input_guess(user_input)
-            except ValueError:
-                print("Please enter a valid number.")
+# Create the frame
+frame = simplegui.create_frame("Guess the Number", 200, 200)
+frame.add_input("Enter your guess:", input_guess, 100)
+frame.add_button("Range is [0,100)", range_100, 200)
+frame.add_button("Range is [0,1000)", range_1000, 200)
 
-if __name__ == "__main__":
-    main()
+# Start the game
+new_game()
+frame.start()
